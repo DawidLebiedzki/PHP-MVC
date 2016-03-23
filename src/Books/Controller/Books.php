1e856b0ec7385f3f4be5946ca1e744387f91d5e7
DawidLebiedzki <lebiedzki@gmail.com>
2016-03-23 21:43:52 +0100

    Framework initial commit

---
+++ src/Books/Controller/Books.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Books\Controller;
+
+class Books {
+
+}
--- web/index.php
+++ web/index.php
@@ -1,33 +1,20 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__ . '/../src/app/routes.php';
+
+$route = include __DIR__ . '/../src/routes.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context); // Pierwszy parametr obikety z naszego pliku routes.php
+$matcher = new Routing\Matcher\UrlMatcher($route, $context);
+$resolver = new HttpKernel\Controller\ControllerResolver();
 
-
-$response = new Response();
-
-try{
-	extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-
-	ob_start();
-	include sprintf(__DIR__. '/../src/app/controllers/%s.php'. $_route);
-	$response = new Response(ob_get_clean());
-
-}catch (Routing\Exception\RouteNotFoundException $e){
-	$response->setStatusCode(404);
-	$response->setContent($e->getMessage());
-}catch (Exception $e){
-	$response->setStatusCode(404);
-	$response->setContent($e->getMessage());
-}
-
+$framework = new Simplex\Framework($matcher, $resolver);
+$response = $framework->handle($request);
+$response->send();
 
